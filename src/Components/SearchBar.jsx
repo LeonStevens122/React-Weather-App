6fdda0c386c3aa5df0c61ab424fb964588432890
Leon Stevens <32592070+LeonStevens122@users.noreply.github.com>
2020-08-19 15:09:20 +0200

    Cleaned up code for deployment

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import ReactSearchBox from 'react-search-box';
+import React, { Component } from "react";
+import ReactSearchBox from "react-search-box";
 
-let cities = require('../Assets/city.list.json');
-
+let cities = require("../Assets/city.list.json");
 
 export default class SearchCities extends Component {
-    data = cities;
+  data = cities;
 
-    render() {
-        console.log("cities object", cities);
+  render() {
     return (
       <ReactSearchBox
         placeholder="Placeholder"
         value="Doe"
-            data={cities}
-        callback={record => console.log("Console Log Search : ",record)}
+        data={cities}
+        callback={(record) => console.log("Console Log Search : ", record)}
       />
-    )
+    );
   }
 }
